ce984802af822b3c98d2616c433f52f1a8b8eaf4
daniel hazan <45601911+danielhazan@users.noreply.github.com>
2018-12-05 14:36:09 +0200

    Create Queen.h

---
+++ Queen.h
@@ -0,0 +1,43 @@
+//
+// Created by daniel hazan on 1/7/2018.
+//
+#include "Piece.h"
+
+#ifndef CPPEX2NEW_QUEEN_H
+#define CPPEX2NEW_QUEEN_H
+
+
+/**
+ * class Queen defines the fields and methods for the Queen piece on chessboard
+ * */
+class Queen : public Piece //Soldier hides Piece
+{
+public:
+    /**
+    * Piece constructor, loading it's inital x position, y position, it's color and T/F whether the piece
+    * is a king or not.
+    * @param xLoc original x pos
+    * @param yLoc original y pos
+    * @param color piece color
+    * @param IsKing T/F
+    */
+    Queen(int xLoc, int yLoc, string color, bool IsKing = false);
+
+    /**
+     * returns the unicode for this piece for printing to the screen by the Board class
+     */
+    string getUniCode() override;
+
+    /**
+     * returns all the piece's possible moves from it's current coords
+     */
+    int*** getAllPossibleMoves() override;
+
+    /**
+     * returns T/F whether the given coords are a valid move for this piece
+     */
+    bool isValidMove(Board &board, int xCord, int yCord) override;
+};
+
+
+#endif //CPPEX2NEW_QUEEN_H
